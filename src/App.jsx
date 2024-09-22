34d2d6adfa07fb0442cec60a52cca805acf9812c
mas <mas154572@gmail.com>
2024-09-22 08:15:36 -0400

    added hover states and reponsiveness to nav css

--- src/App.jsx
+++ src/App.jsx
@@ -13,35 +13,67 @@ function Navbar() {
     <header className="flex justify-center">
       <nav className="flex justify-between max-w-[1400px] w-full p-2 items-center border-b-dark-white-color border-b-2">
         <img src="/public/CoolClothes.com.svg" alt="" />
-        <ul className="flex">
+        <ul className=" hidden desktop:flex">
           <li className="mx-8">
-            <a href="#">Kids</a>
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Kids
+            </a>
           </li>
           <li className="mx-8">
-            <a href="#">Women</a>
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Women
+            </a>
           </li>
           <li className="mx-8">
-            <a href="#">Men</a>
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Men
+            </a>
           </li>
           <li className="mx-8">
-            <a href="#">Accesories</a>
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Accesories
+            </a>
           </li>
           <li className="mx-8">
-            <a href="#">Contact</a>
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Contact
+            </a>
           </li>
         </ul>
         <div>
-          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color"></button>
-          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color ml-4"></button>
+          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"></button>
+          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color ml-4 hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"></button>
         </div>
+        <button className="desktop:hidden">
+          <img src="public/menu.svg" alt="menu icon" />
+        </button>
       </nav>
     </header>
   );
 }
 
-function Main() {}
+function Main() {
+  return <section></section>;
+}
 
-function Photos() {}
+function Photos() {
+  return <section></section>;
+}
 
 function Pricing() {}
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      screens: {
-        desktop: "640px",
-        // => @media (min-width: 640px) { ... }
+    screens: {
+      desktop: "1200px",
+      // => @media (min-width: 640px) { ... }
 
-        mobile: "1024px",
-        // => @media (min-width: 1024px) { ... }
-      },
+      mobile: "0px",
+      // => @media (min-width: 1024px) { ... }
+    },
+    extend: {
       colors: {
         "background-color": "#fff",
         "text-color": "#1A1A1A",
         "dark-white-color": "#F2F2F2",
         "grey-color": "#CCCCCC",
-        "red-color": "##FF5959",
+        "red-color": "#FF5959",
       },
     },
   },
