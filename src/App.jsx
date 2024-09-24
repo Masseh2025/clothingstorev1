4d7e2590ffd3329f4df27fcb543b0418e11ff28d
mas <mas154572@gmail.com>
2024-09-23 22:58:50 -0400

    Finished responsive nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <main></main>
     </div>
   );
 }
 
 function Navbar() {
+  const [menu, setMenu] = useState(false);
+  function handleMenu() {
+    setMenu(!menu);
+  }
   return (
-    <header className="flex justify-center">
+    <header className="flex items-center flex-col relative">
+      <a href="#" className="skip-nav">
+        Skip to main content
+      </a>
       <nav className="flex justify-between max-w-[1400px] w-full p-2 items-center border-b-dark-white-color border-b-2">
         <img src="/public/CoolClothes.com.svg" alt="" />
         <ul className=" hidden desktop:flex">
@@ -56,19 +64,95 @@ function Navbar() {
           </li>
         </ul>
         <div>
-          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"></button>
-          <button className="rounded-full w-[48px] h-[48px] bg-dark-white-color ml-4 hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"></button>
+          <button
+            className="rounded-full w-[48px] h-[48px] bg-dark-white-color hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"
+            aria-label="shopping cart"
+          ></button>
+          <button
+            className="rounded-full w-[48px] h-[48px] bg-dark-white-color ml-4 hover:bg-grey-color focus-visible:bg-grey-color hidden desktop:inline"
+            aria-label="Account menu"
+          ></button>
         </div>
-        <button className="desktop:hidden">
+        <button
+          className="desktop:hidden"
+          aria-expanded={menu}
+          onClick={() => handleMenu()}
+        >
           <img src="public/menu.svg" alt="menu icon" />
         </button>
+      </nav>
+      <nav className="w-full">
+        <ul
+          className={
+            menu
+              ? "flex flex-col bg-dark-white-color w-full items-center"
+              : "hidden"
+          }
+        >
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Profile
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Shopping cart
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Kids
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Women
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Men
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Accesories
+            </a>
+          </li>
+          <li className="mx-8">
+            <a
+              className="hover:text-red-color focus-visible:text-red-color"
+              href="#"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
       </nav>
     </header>
   );
 }
 
 function Main() {
-  return <section></section>;
+  return <main></main>;
 }
 
 function Photos() {
